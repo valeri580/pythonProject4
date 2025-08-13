53ea05c2dbfc66f5f9139725d435fb25516bf0e6
Валерий <valeriperehodov@gmail.com>
2025-08-13 23:39:43 +0300

    Initial project upload

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "Never-say-never"
+
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
